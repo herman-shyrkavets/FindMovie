ff07789c6eddc43aad2a4d65ff458750b255cef7
herman_shyrkavets <gsiruovec@gmail.com>
2026-01-28 18:23:25 +0300

    update name for directory

--- src/app_layer/use_cases/__init__.py
+++ src/app_layer/use_cases/__init__.py
@@ -1,5 +1,5 @@
 from src.app_layer.use_cases.request_logs_service import RequestLogsService
-from src.app_layer.use_cases.muvie_service import MovieService
+from src.app_layer.use_cases.movie_service import MovieService
 
 
 __all__ = ["RequestLogsService", "MovieService"]
